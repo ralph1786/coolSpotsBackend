5630be5714738fca07ae72b5029872e6474e0307
ralph1786 <ralph1786@gmail.com>
2019-04-10 12:51:45 -0400

    fix spelling error, added render to update action

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -1,32 +1,35 @@
+# frozen_string_literal: true
+
 class Api::V1::SpotsController < ApplicationController
-    def index 
-    @spots = Spot.all   
-    render json: @spots  
-    end
+  def index
+    @spots = Spot.all
+    render json: @spots
+  end
 
-    def show # getting sounds by location ID
-        @spot = Spot.find(params[:id])
-        render json: @spot
-    end
+  def show # getting sounds by location ID
+    @spot = Spot.find(params[:id])
+    render json: @spot
+  end
 
-    def create
-        @spot = Spot.create!(spot_params)
-        render json: @sound
-    end
+  def create
+    @spot = Spot.create!(spot_params)
+    render json: @spot
+  end
 
-    def update
-	    @spot = Spot.find(params[:id]) 
-        @spot.update(spot_params) 
-    end
+  def update
+    @spot = Spot.find(params[:id])
+    @spot.update(spot_params)
+    render json: @spot
+  end
 
-    def destroy
-	    @spot = Spot.find(params[:id]) 
-        @spot.destroy
-    end
+  def destroy
+    @spot = Spot.find(params[:id])
+    @spot.destroy
+  end
 
-    private
+  private
 
-    def spot_params
-        params.permit(:name, :address, :review, :image, :location_id)
-    end
+  def spot_params
+    params.permit(:name, :address, :review, :image, :location_id)
+  end
 end
